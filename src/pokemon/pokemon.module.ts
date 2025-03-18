960918a08d295ac689967f934adb5fcd4f432d6d
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-03-18 19:01:52 -0300

    chore: criação da service

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
 
-@Module({})
+@Module({
+  providers: [PokemonService]
+})
 export class PokemonModule {}
