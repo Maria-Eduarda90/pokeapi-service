0a1e9a188ed5254cc0f060fc6cf1ea13578983e6
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-03-18 19:18:44 -0300

    criação da controller, buscar as informações via metodo http GET

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PokemonDTO } from './dto/pokemon.dto';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
-export class PokemonController {}
+export class PokemonController {
+    constructor(private readonly pokemonService: PokemonService) { }
+
+    @Get('fire')
+    async getFirePokemons(): Promise<PokemonDTO[]> {
+        return this.pokemonService.getFirePokemons();
+    }
+}
