bcb73e02a2f63fbd94987db3f380833b61101481
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-03-18 19:27:30 -0300

    feat: swagger adicionado para documentar os endpoints

--- src/pokemon/dto/pokemon.dto.ts
+++ src/pokemon/dto/pokemon.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class PokemonDTO {
+    @ApiProperty()
     name: string;
+
+    @ApiProperty({ type: [String] })
     types: string[];
+
+    @ApiProperty({ type: [String] })
     abilities: string[];
 }
--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { PokemonDTO } from './dto/pokemon.dto';
 import { PokemonService } from './pokemon.service';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('pokemon')
 export class PokemonController {
     constructor(private readonly pokemonService: PokemonService) { }
 
     @Get('fire')
+    @ApiOkResponse({ description: 'Lista de pokémons do tipo Fire', type: [PokemonDTO] })
     async getFirePokemons(): Promise<PokemonDTO[]> {
         return this.pokemonService.getFirePokemons();
     }
